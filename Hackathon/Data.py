73ce538c48603fde5615f4066d3acbe3f2444520
BenBerol <benberol40@gmail.com>
2024-04-15 09:50:44 -0400

    Add hackathon project

---
+++ Hackathon/Data.py
@@ -0,0 +1,24 @@
+class Data:
+    def __init__(self, date=0, hai=0, medianIncome=0, medianHousePrice=0, interestRates=0, population=0, unemploymentRate=0):
+        self.date = date
+        self.hai = hai
+        self.medianIncome = medianIncome
+        self.medianHousePrice = medianHousePrice
+        self.interestRates = interestRates
+        self.population = population
+        self.unemploymentRate = unemploymentRate\
+        
+    def to_dict(self):
+        return {
+            "date": self.date,
+            "hai": self.hai,
+            "medianIncome": self.medianIncome,
+            "medianHousePrice": self.medianHousePrice,
+            "interestRates": self.interestRates,
+            "population": self.population,
+            "unemploymentRate": self.unemploymentRate
+        }
+    
+    @classmethod
+    def from_dict(cls, data_dict):
+        return cls(**data_dict)
