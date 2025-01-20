b2ab9137071baa4bfb5c9f0461ec749342eec7d6
BenBerol <benberol40@gmail.com>
2025-01-19 21:04:31 -0500

    Create a preview of the path of the robot with adjustable speed

--- CS_IA/field.py
+++ CS_IA/field.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import math
 import path_points as pp
+import time as tm
 
 class Field:
 
@@ -18,8 +19,8 @@ class Field:
     - clear_robot(): Removes the robot from the field
     - clear_lines(): Removes the path from the field
     - clear_canvas(): Removes all drawings from the canvas
-
-
+    - clear_previews(): Removes all robot previews from the field
+    - run_preview(speed): Runs a preview of the robot following the path at the given speed
 
     Parameters:
     - frame (tk.Frame): The frame to place the field in
@@ -34,7 +35,10 @@ class Field:
         self.robot_center = None
         self.x_offset = None
         self.y_offset = None
+        self.path = []
         self.lines = []
+        self.previews = []
+        self.preview_number = 0
     
     def draw_grid(self, width, height):
         if width > height:
@@ -54,9 +58,7 @@ class Field:
         for i in range(self.y_offset, self.y_offset + grid_height + 1, self.size_factor):
             self.canvas.create_line(self.x_offset, i, self.x_offset + grid_width, i, fill="black", tags="grid")
 
-    def draw_robot(self, x, y, theta):
-        self.clear_robot()
-
+    def draw_robot(self, x, y, theta, preview):
         x *= self.size_factor
         y *= self.size_factor
 
@@ -77,29 +79,40 @@ class Field:
             rotated_y = corner[0] * math.sin(theta_rad) + corner[1] * math.cos(theta_rad)
             rotated_corners.append((rotated_x + x + self.x_offset, rotated_y + y + self.y_offset))
 
-        self.robot_image = self.canvas.create_polygon(rotated_corners, fill="blue", tags="robot")
-        if (robot_x < robot_y): 
-            self.robot_center = self.canvas.create_oval(
-                x - robot_x / 6 + self.x_offset, y - robot_x / 6 + self.y_offset, 
-                x + robot_x / 6 + self.x_offset, y + robot_x / 6 + self.y_offset, 
-                fill="red", tags="robot_center")
+        if preview is False:
+            self.clear_robot()
+            self.robot_image = self.canvas.create_polygon(rotated_corners, fill="blue", outline="blue", tags="robot")
+            if (robot_x < robot_y): 
+                self.robot_center = self.canvas.create_oval(
+                    x - robot_x / 6 + self.x_offset, y - robot_x / 6 + self.y_offset, 
+                    x + robot_x / 6 + self.x_offset, y + robot_x / 6 + self.y_offset, 
+                    fill="red", tags="robot_center")
+            else:
+                self.robot_center = self.canvas.create_oval(
+                    x - robot_y / 6 + self.x_offset, y - robot_y / 6 + self.y_offset, 
+                    x + robot_y / 6 + self.x_offset, y + robot_y / 6 + self.y_offset, 
+                    fill="red", tags="robot_center")
         else:
-            self.robot_center = self.canvas.create_oval(
-                x - robot_y / 6 + self.x_offset, y - robot_y / 6 + self.y_offset, 
-                x + robot_y / 6 + self.x_offset, y + robot_y / 6 + self.y_offset, 
-                fill="red", tags="robot_center")
+            robot_preview = self.canvas.create_polygon(rotated_corners, fill="", outline="blue", dash=(5,2), tags="robot")
+            self.previews.append(robot_preview)
         
-        print(f"Robot dimensions: {robot_x}, {robot_y}")
-
     def draw_path(self, path):
-        if not path or len(path) <= 1:
+        self.path = path
+
+        if not self.path or len(self.path) <= 1:
             return
         
         self.clear_lines()
+        self.clear_previews()
+
+        for point in self.path:
+            if point == self.path[0]:
+                continue
+            self.draw_robot(point.get_x(), point.get_y(), point.get_angle(), True)
 
         scaled_path = [(point.get_x() * self.size_factor + self.x_offset, 
                         point.get_y() * self.size_factor + self.y_offset) 
-                        for point in path]
+                        for point in self.path]
 
         for i in range(len(scaled_path) - 1):
             line = self.canvas.create_line(
@@ -114,11 +127,42 @@ class Field:
         self.canvas.tag_raise("path")
         self.canvas.tag_raise("robot_center")
 
+    def run_preview(self, speed):
+        self.preview_number += 1
+        preview_number = self.preview_number
+        line_lengths = []
+        path_length = 0
+        for line in self.lines:
+            x0, y0, x1, y1 = self.canvas.coords(line)
+            length = math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+            line_lengths.append(length)
+            path_length += length
+        pixPerSec = (int)(path_length/speed)
+        for i in range(0, len(self.path) - 1):
+            x_dif = self.path[i+1].get_x() - self.path[i].get_x()
+            y_dif = self.path[i+1].get_y() - self.path[i].get_y()
+            angle = self.path[i+1].get_angle() - self.path[i].get_angle()
+            time = (line_lengths[i]/pixPerSec)
+            for j in range(0, (int)(50*time+1)):
+                if (self.preview_number != preview_number):
+                    return
+                self.draw_robot(self.path[i].get_x() + x_dif*j/time/50, self.path[i].get_y() + y_dif*j/time/50, self.path[i].get_angle() + angle*j/time/50, False)
+                start_time = tm.time()
+                self.canvas.update()
+                elapsed_time = tm.time() - start_time
+                delay = max(0, 19 - elapsed_time*1000)
+                self.canvas.after(int(delay))
+        self.draw_robot(self.path[-1].get_x(), self.path[-1].get_y(), self.path[-1].get_angle(), False)
+
     def clear_robot(self):
         if self.robot_image:
             self.canvas.delete(self.robot_image)
         if self.robot_center:
             self.canvas.delete(self.robot_center)
+
+    def clear_previews(self):
+        for preview in self.previews:
+            self.canvas.delete(preview)
     
     def clear_lines(self):
         for line in self.lines:
@@ -130,7 +174,6 @@ class Field:
 
     def set_robot(self, robot):
         self.robot = robot
-        print(f"Robot set: {self.robot._robot_name}")
 
     def get_robot(self):
         return self.robot
